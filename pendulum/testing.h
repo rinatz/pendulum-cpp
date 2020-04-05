f5c4d5213578352cfed7e3bb35f6b1e235ab79ce
Ida Kenichiro <ida.kenichiro@gmail.com>
2020-04-05 12:22:27 +0900

    Change Optional<T>

--- pendulum/testing.h
+++ pendulum/testing.h
@@ -2,25 +2,25 @@
 #define PENDULUM_TESTING_H_
 
 #include <functional>
-#include <memory>
 
 #include "pendulum/datetime.h"
+#include "pendulum/optional.h"
 
 namespace pendulum {
 
 namespace internal {
 
-inline std::unique_ptr<DateTime>& test_now() {
-    static std::unique_ptr<DateTime> dt;
+inline Optional<DateTime>& test_now() {
+    static Optional<DateTime> dt;
     return dt;
 }
 
 }  // namespace internal
 
-inline bool has_test_now() { return internal::test_now() != nullptr; }
-inline DateTime get_test_now() { return *internal::test_now(); }
-inline void set_test_now(const DateTime& now) { internal::test_now().reset(new DateTime(now)); }
-inline void set_test_now() { internal::test_now().reset(); }
+inline bool has_test_now() { return internal::test_now().has_value(); }
+inline DateTime get_test_now() { return internal::test_now().value(); }
+inline void set_test_now(const DateTime& now) { internal::test_now() = now; }
+inline void set_test_now() { internal::test_now() = nullopt; }
 
 inline void test(const DateTime& now, std::function<void()> func) {
     set_test_now(now);
